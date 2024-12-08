c9f3ea54683cfa924df1f1f229eddafcc50b74ff
Dilantha Jayathiratne <jatzuk.22@gmail.com>
2024-12-08 03:48:00 +0100

    day 07 solutions

---
+++ src/main/kotlin/dev/jatzuk/aoc/y2024/day07/D7P1.kt
@@ -0,0 +1,28 @@
+package dev.jatzuk.aoc.y2024.day07
+
+class D7P1 {
+
+  fun solution(input: List<String>): Long {
+    return input.sumOf {
+      val target = it.substringBefore(":").toLong()
+      val operands = it.substringAfter(" ")
+        .split(" ")
+        .map(String::toLong)
+
+      val isSolvable = isSolvable(target, operands, 0, operands[0])
+      if (isSolvable) target else 0L
+    }
+  }
+
+  private fun isSolvable(target: Long, operands: List<Long>, index: Int, current: Long): Boolean {
+    if (index == operands.lastIndex) {
+      return current == target
+    }
+
+    val next = operands[index + 1]
+
+    val addition = isSolvable(target, operands, index + 1, current + next)
+    val multiplication = isSolvable(target, operands, index + 1, current * next)
+    return addition || multiplication
+  }
+}
