a65a60c47894b5e19b4ad3344d0bd0908109f4af
Dilantha Jayathiratne <jatzuk.22@gmail.com>
2024-12-16 11:03:27 +0100

    move helper function below the tests

--- src/test/kotlin/dev/jatzuk/aoc/y2024/day01/D1P1Test.kt
+++ src/test/kotlin/dev/jatzuk/aoc/y2024/day01/D1P1Test.kt
@@ -6,32 +6,33 @@ import io.kotest.matchers.shouldBe
 
 class D1P1Test : FunSpec({
 
-  fun getInputs(path: String): Pair<List<Long>, List<Long>> {
-    val left = mutableListOf<Long>()
-    val right = mutableListOf<Long>()
-
-    path.fromResourcesReadLines()
-      .map {
-        val (leftNumber, rightNumber) = it.split("\\s".toRegex()).filter(String::isNotEmpty)
-        leftNumber.toLong() to rightNumber.toLong()
-      }
-      .forEach {
-        left.add(it.first)
-        right.add(it.second)
-      }
-
-    return left to right
-  }
-
   test("example") {
-    val (left, right) = getInputs("/y2024/day01/part1/input0")
+    val (left, right) = getInput("/y2024/day01/part1/input0")
     val result = D1P1().solution(left.toLongArray(), right.toLongArray())
     result.shouldBe(11)
   }
 
   test("solution") {
-    val (left, right) = getInputs("/y2024/day01/part1/input1")
+    val (left, right) = getInput("/y2024/day01/part1/input1")
     val result = D1P1().solution(left.toLongArray(), right.toLongArray())
     result.shouldBe(2192892)
   }
 })
+
+private fun getInput(path: String): Pair<List<Long>, List<Long>> {
+  val left = mutableListOf<Long>()
+  val right = mutableListOf<Long>()
+
+  path.fromResourcesReadLines()
+    .map {
+      val (leftNumber, rightNumber) = it.split("\\s".toRegex()).filter(String::isNotEmpty)
+      leftNumber.toLong() to rightNumber.toLong()
+    }
+    .forEach {
+      left.add(it.first)
+      right.add(it.second)
+    }
+
+  return left to right
+}
+
